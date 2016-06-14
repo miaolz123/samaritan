dda0d076d96564a083d962244a345066df31dd67
miaolz123 <miaolizhao@126.com>
2016-06-14 23:42:23 +0800

    sleep save

--- api/okcoin.cn.go
+++ api/okcoin.cn.go
@@ -11,19 +11,21 @@ import (
 
 // OKCoinCn : the exchange struct of okcoin.cn
 type OKCoinCn struct {
-	stockMap map[string]string
-	host     string
-	log      log.Logger
-	option   Option
+	stockMap     map[string]string
+	orderTypeMap map[string]int
+	host         string
+	log          log.Logger
+	option       Option
 }
 
 // NewOKCoinCn : create an exchange struct of okcoin.cn
 func NewOKCoinCn(opt Option) *OKCoinCn {
 	e := OKCoinCn{
-		stockMap: map[string]string{"BTC": "btc", "LTC": "ltc"},
-		host:     "https://www.okcoin.cn/api/v1/",
-		log:      log.New(opt.Type),
-		option:   opt,
+		stockMap:     map[string]string{"BTC": "btc", "LTC": "ltc"},
+		orderTypeMap: map[string]int{"buy": 1, "sell": -1, "buy_market": 2, "sell_market": -2},
+		host:         "https://www.okcoin.cn/api/v1/",
+		log:          log.New(opt.Type),
+		option:       opt,
 	}
 	if _, ok := e.stockMap[e.option.MainStock]; !ok {
 		e.option.MainStock = "BTC"
@@ -159,4 +161,79 @@ func (e *OKCoinCn) Sell(stockType string, price, amount float64, msgs ...interfa
 	e.log.Do("sell", price, amount, msgs...)
 	id = json.Get("order_id").MustInt()
 	return
+}
+
+// CancelOrder ...
+func (e *OKCoinCn) CancelOrder(order map[string]interface{}) bool {
+	params := []string{
+		"api_key=" + e.option.AccessKey,
+		"symbol=" + e.stockMap[fmt.Sprint(order["StockType"])] + "_cny",
+		fmt.Sprint("order_id=", conver.IntMust(order["Id"])),
+	}
+	params = append(params, "sign="+strings.ToUpper(signMd5(params)))
+	resp, err := post(e.host+"cancel_order.do", params)
+	if err != nil {
+		e.log.Do("error", 0.0, 0.0, "CancelOrder() error, ", err)
+		return false
+	}
+	json, err := simplejson.NewJson(resp)
+	if err != nil {
+		e.log.Do("error", 0.0, 0.0, "CancelOrder() error, ", err)
+		return false
+	}
+	if result := json.Get("result").MustBool(); !result {
+		e.log.Do("error", 0.0, 0.0, "CancelOrder() error, the error number is ", json.Get("error_code").MustInt())
+		return false
+	}
+	e.log.Do("cancel", 0.0, 0.0, fmt.Sprintf("%v", order))
+	return true
+}
+
+// GetOrders ...
+func (e *OKCoinCn) GetOrders(stockType string) (orders []map[string]interface{}) {
+	params := []string{
+		"api_key=" + e.option.AccessKey,
+		"symbol=" + e.stockMap[stockType] + "_cny",
+		"status=0",
+		"current_page=1",
+		"page_length=200",
+	}
+	params = append(params, "sign="+strings.ToUpper(signMd5(params)))
+	resp, err := post(e.host+"order_history.do", params)
+	if err != nil {
+		e.log.Do("error", 0.0, 0.0, "GetOrders() error, ", err)
+		return
+	}
+	json, err := simplejson.NewJson(resp)
+	if err != nil {
+		e.log.Do("error", 0.0, 0.0, "GetOrders() error, ", err)
+		return
+	}
+	if result := json.Get("result").MustBool(); !result {
+		e.log.Do("error", 0.0, 0.0, "GetOrders() error, the error number is ", json.Get("error_code").MustInt())
+		return
+	}
+	ordersJSON := json.Get("orders")
+	count := len(ordersJSON.MustArray())
+	for i := 0; i < count; i++ {
+		orderJSON := ordersJSON.GetIndex(i)
+		order := map[string]interface{}{
+			"Id":         orderJSON.Get("order_id").MustInt(),
+			"Price":      orderJSON.Get("price").MustFloat64(),
+			"Amount":     orderJSON.Get("amount").MustFloat64(),
+			"DealAmount": orderJSON.Get("deal_amount").MustFloat64(),
+			"OrderType":  e.orderTypeMap[orderJSON.Get("type").MustString()],
+			"StockType":  stockType,
+		}
+		orders = append(orders, order)
+	}
+	return orders
+}
+
+// GetOrder ...
+func (e *OKCoinCn) GetOrder() map[string]interface{} {
+	return map[string]interface{}{
+		"Id":        123456789,
+		"StockType": "BTC",
+	}
 }
